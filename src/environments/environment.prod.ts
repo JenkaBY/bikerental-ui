34cb87596eae698b5092d02c3c2124b9cbc015a3
Yauhen <evgen.kuzmich@gmail.com>
2026-03-06 16:56:41 +0200

    fix: use ru locale

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   healthPollIntervalMs: 300_000,
-  defaultLocale: 'ru-RU',
+  defaultLocale: 'ru',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
   healthPollIntervalMs: 300_000,
-  defaultLocale: 'ru-RU',
+  defaultLocale: 'ru',
 };
