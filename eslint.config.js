b9037ac3f119348b2040423cfd4a37f97f58ad53
Yauhen <evgen.kuzmich@gmail.com>
2026-04-19 09:28:22 +0300

    feat: add generated services and models

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,49 @@
 // @ts-check
 const eslint = require('@eslint/js');
-const {defineConfig} = require('eslint/config');
+const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const prettier = require('eslint-config-prettier');
 const prettierPlugin = require('eslint-plugin-prettier');
 
-module.exports = defineConfig([
-  {
-    files: ['**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
-      prettier,
-    ],
-    plugins: {prettier: prettierPlugin},
-    processor: angular.processInlineTemplates,
-    rules: {
-      'prettier/prettier': 'error',
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
+module.exports = defineConfig({
+  ignorePatterns: ['src/app/core/api/generated/**'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: [
+        eslint.configs.recommended,
+        tseslint.configs.recommended,
+        tseslint.configs.stylistic,
+        angular.configs.tsRecommended,
+        prettier,
       ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+      plugins: { prettier: prettierPlugin },
+      processor: angular.processInlineTemplates,
+      rules: {
+        'prettier/prettier': 'error',
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
+        ],
+      },
     },
-  },
-  {
-    files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
-  },
-]);
-
+    {
+      files: ['**/*.html'],
+      extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+      rules: {},
+    },
+  ],
+});
