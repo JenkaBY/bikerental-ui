bbde640a7db708cd50f8934a94ac377aa59ed9b6
jenkaBY <evgen.kuzmich@gmail.com>
2026-07-02 16:52:06 +0300

    feat: after login redirect to original url

--- projects/shared/src/core/auth/auth.guard.ts
+++ projects/shared/src/core/auth/auth.guard.ts
@@ -2,11 +2,11 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (_route, state) => {
   const auth = inject(AuthService);
   if (auth.isAuthenticated()) {
     return true;
   }
-  auth.login();
+  auth.login(state.url);
   return false;
 };
